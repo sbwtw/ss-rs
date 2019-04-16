ee1782c95b3e7577755ebdb8e18f715f9a6fbd88
石博文 <sbw@sbw.so>
2019-04-16 20:45:39 +0800

    handshake finished

--- src/main.rs
+++ src/main.rs
@@ -3,276 +3,145 @@ use futures::{Future, Async, Poll};
 use futures::try_ready;
 use futures::sync::mpsc;
 use futures::sink::Sink;
-use tokio::prelude::AsyncWrite;
+use tokio::prelude::*;
 use tokio::prelude::stream::Stream;
+use tokio::io::{read_exact, write_all};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::codec::{Encoder, Decoder, Framed};
 use bytes::BytesMut;
 use bytes::buf::BufMut;
+use failure::Fail;
 
-use std::net::{SocketAddr, IpAddr};
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
 use std::sync::{Arc, Mutex};
-use std::collections::HashMap;
-use std::cell::Cell;
 
-#[derive(Clone, Debug)]
-enum RequestAddr {
-    Domain(String),
-    Ipv4(u32),
-    //Ipv6([u32; 4]),
+#[derive(Debug, Fail)]
+enum HandshakeError {
+    #[fail(display = "Socks5 Version Error: 0x{:02x}", version)]
+    VersionError {
+        version: u8,
+    },
+    #[fail(display = "Only method type 0x00 is supported, server request one of {:x?}", supported)]
+    MethodError {
+        supported: Vec<u8>,
+    },
 }
 
-#[derive(Clone, Debug)]
-struct Request {
-    ver: u8,
-    cmd: u8,
-    _rsv: u8,
-    atyp: u8,
-    dst_port: u16,
-    dst_addr: RequestAddr,
-}
-
-impl Request {
-    fn new(buf: &mut BytesMut) -> Option<Self> {
-        let ver = buf[0];
-        let cmd = buf[1];
-        let _rsv = buf[2];
-        let atyp = buf[3];
-
-        let dst_addr = match atyp {
-            0x01 => {
-                let ip = u32::from_be_bytes([buf[4], buf[5], buf[6], buf[7]]);
-                let _ = buf.split_to(4);
-                RequestAddr::Ipv4(ip)
-            },
-            0x03 => {
-                let len = buf[4] as usize;
-                let _ = buf.split_to(5);
-                let domain = buf.split_to(len);
-                let domain = std::str::from_utf8(&domain[..]).unwrap();
-                RequestAddr::Domain(domain.to_string())
-            },
-            //0x04 => {},
-
-            _ => { println!("ERROR when parse request"); return None; }
-        };
-
-        let dst_port = u16::from_be_bytes([buf[0], buf[1]]);
-
-        Some(Self {
-            ver,
-            cmd,
-            _rsv,
-            atyp,
-            dst_addr,
-            dst_port,
-        })
-    }
-}
-
-#[derive(Clone, Debug)]
-enum SocksMsgV5 {
-    Handshake,
-    Request(Request),
-    Forward(BytesMut),
-}
-
-enum State {
-    Init,
-    Handshake,
-    Forward,
-}
-
-struct SocksCodecV5 {
-    state: State,
-}
-
-impl SocksCodecV5 {
-    pub fn new() -> Self {
-        Self {
-            state: State::Init,
+fn handshake(socket: TcpStream) -> impl Future<Item = TcpStream, Error = failure::Error> {
+    // read socks version
+    read_exact(socket, [0u8])
+        .map_err(|e| e.into())
+        .and_then(|(socket, buf)| {
+        if buf[0] != 0x05 {
+            Err(HandshakeError::VersionError { version: buf[0] }.into())
+        } else {
+            Ok(socket)
         }
-    }
-}
-
-impl Encoder for SocksCodecV5 {
-    type Item = Vec<u8>;
-    type Error = failure::Error;
-
-    fn encode(&mut self, src: Self::Item, buf: &mut BytesMut) -> Result<(), Self::Error> {
-        buf.reserve(src.len());
-        buf.put(&src[..]);
-        Ok(())
-    }
-}
-
-impl Decoder for SocksCodecV5 {
-    type Item = SocksMsgV5;
-    type Error = failure::Error;
-
-    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        if buf.len() == 0 {
-            return Ok(None);
-        }
-
-        match self.state {
-            State::Init => {
-                buf.clear();
-                self.state = State::Handshake;
-                Ok(Some(SocksMsgV5::Handshake))
-            },
-            State::Handshake => {
-                if let Some(r) = Request::new(buf) {
-                    self.state = State::Forward;
-                    buf.clear();
-                    Ok(Some(SocksMsgV5::Request(r)))
+    })
+    // read method length
+    .and_then(|socket| {
+        read_exact(socket, [0u8])
+            .map_err(|e| e.into())
+            .and_then(|(socket, buf)| {
+                Ok((socket, buf[0] as usize))
+            })
+    })
+    // read method list
+    .and_then(|(socket, method_len)| {
+        read_exact(socket, vec![0u8; method_len])
+            .map_err(|e| e.into())
+            .and_then(|(socket, buf)| {
+                if buf.contains(&0x00) {
+                    Ok(socket)
                 } else {
-                    panic!()
+                    Err(HandshakeError::MethodError { supported: buf }.into())
                 }
-            },
-            _ => {
-                Ok(Some(SocksMsgV5::Forward(buf.take())))
+            })
+    })
+    // write method select response
+    .and_then(|socket| {
+        write_all(socket, [0x05, 0x00])
+            .map_err(|e| e.into())
+    })
+    .and_then(|(socket, _)| {
+        Ok(socket)
+    })
+}
+
+fn create_connection(socket: TcpStream) -> impl Future<Item = (TcpStream, TcpStream), Error = failure::Error> {
+    read_exact(socket, [0u8; 4])
+        .map_err(|e| e.into())
+        .and_then(|(socket, buf)| {
+            assert!(buf[0] == 0x05 && buf[1] == 0x01 && buf[2] == 0x00);
+            Ok((socket, buf[3]))
+        })
+    .and_then(|(socket, atyp)| {
+        match atyp {
+            0x01 => {
+                read_exact(socket, [0u8; 4])
+                    .map_err(|e| e.into())
+                    .and_then(|(socket, buf)| {
+                        let ip = Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]);
+                        Ok((socket, IpAddr::V4(ip)))
+                    })
             }
+            _ => panic!(),
         }
-    }
+    })
+    .and_then(|(socket, ip)| {
+        read_exact(socket, [0u8; 2])
+            .map_err(|e| e.into())
+            .and_then(move |(socket, buf)| {
+                let port = u16::from_be_bytes([buf[0], buf[1]]);
+                Ok((socket, SocketAddr::new(ip, port)))
+            })
+    })
+    .and_then(|(socket, addr)| {
+        TcpStream::connect(&addr)
+            .map_err(|e| e.into())
+            .and_then(move |serv_socket| Ok((socket, serv_socket)))
+    })
 }
 
-struct Shared {
-    peers: HashMap<SocketAddr, mpsc::UnboundedSender<String>>,
+struct Transfer {
 }
 
-impl Shared {
-    pub fn new() -> Self {
-        Self {
-            peers: HashMap::new(),
-        }
-    }
-}
-
-struct Client {
-    sender: mpsc::UnboundedSender<SocketAddr>,
-    receiver: mpsc::UnboundedReceiver<SocketAddr>,
-    shared: Arc<Mutex<Shared>>,
-    framed: Cell<Framed<TcpStream, SocksCodecV5>>,
-}
-
-impl Client {
-    pub fn new(socket: TcpStream, shared: Arc<Mutex<Shared>>) -> Self {
-
-        //let addr = socket.peer_addr().unwrap();
-        let (tx, rx) = mpsc::unbounded();
-        let framed = SocksCodecV5::new().framed(socket);
-        //shared.lock().unwrap().peers.insert(addr, tx);
-
-        Self {
-            sender: tx,
-            receiver: rx,
-            shared,
-            framed: Cell::new(framed),
-        }
-    }
-
-    //fn peer_addr(&self) -> SocketAddr {
-        //self.framed.get_ref().peer_addr().unwrap()
-    //}
-}
-
-impl Drop for Client {
-    fn drop(&mut self) {
-        //let addr = self.peer_addr();
-        //self.shared.lock().unwrap().peers.remove(&addr);
-    }
-}
-
-impl Future for Client {
+impl Future for Transfer {
     type Item = ();
     type Error = failure::Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        //let peer_addr = self.peer_addr();
-
-        // read from channel and write to socket
-        while let Async::Ready(r) = self.receiver.poll().unwrap() {
-            match r {
-                Some(s) => {
-                    let mut buf: Vec<u8> = vec![0x05, 0x00, 0x00, 0x01];
-                    match s.ip() {
-                        IpAddr::V4(ipv4) => {
-                            buf.append(&mut ipv4.octets().to_vec());
-                        },
-                        _ => { panic!(); },
-                    }
-                    let port: u16 = s.port();
-                    buf.push(((port >> 8) & 0xff) as u8);
-                    buf.push(((port     ) & 0xff) as u8);
-                    println!("Transfer addr: {:?}", s);
-                    self.framed.get_mut().start_send(buf)?;
-                    self.framed.get_mut().poll_complete()?;
-                },
-                _ => {},
-            }
-        }
-
-        while let Async::Ready(item) = self.framed.get_mut().poll()? {
-            match item {
-                Some(SocksMsgV5::Handshake) => {
-                    self.framed.get_mut().start_send(vec![b'\x05', b'\x00'])?;
-                },
-                Some(SocksMsgV5::Request(r)) => {
-                    println!("Request: {:?}", r);
-                    let tx = self.sender.clone();
-                    let socket = self.framed.take().into_inner();
-                    let transfer = TcpStream::connect(&"61.135.169.121:80".parse().unwrap()).map(move |socket| {
-                        tx.unbounded_send(socket.local_addr().unwrap()).unwrap();
-                    });
-
-                    tokio::spawn(transfer.map_err(|_| ()));
-                    return Ok(Async::Ready(()));
-                },
-                Some(SocksMsgV5::Forward(buf)) => {
-                    println!("Forward: {:?}", buf);
-                }
-                _ => {
-                    println!("ERROR when recv");
-                    return Ok(Async::Ready(()));
-                }
-            }
-
-            self.framed.get_mut().poll_complete()?;
-        }
-
-        Ok(Async::NotReady)
+        Ok(Async::Ready(()))
     }
 }
 
-//struct Transfer {
-    //socket: TcpStream,
-//}
+impl Transfer {
+    pub fn new(socket: TcpStream, serv_socket: TcpStream) -> Self {
+        println!("relay {:?} --> {:?} --> {:?} --> {:?}", socket.local_addr(), socket.peer_addr(), serv_socket.local_addr(), serv_socket.peer_addr());
 
-//impl Transfer {
-    //pub fn new(req: &Request) -> Self {
-        //let socket = TcpStream::connect("61.135.169.121:80");
-
-        //Self {
-            //socket,
-        //}
-    //}
-//}
+        Self {
+        }
+    }
+}
 
 fn main() {
-    let shared = Arc::new(Mutex::new(Shared::new()));
-
     let addr: SocketAddr = "127.0.0.1:2000".parse().unwrap();
     let f = TcpListener::bind(&addr)
         .unwrap()
         .incoming()
         .map_err(|e| println!("Incoming Error: {:?}", e))
         .for_each(move |socket| {
-        println!("NEW connection: {:?}", socket.peer_addr());
 
-        let peer = Client::new(socket, shared.clone());
+        let serv = handshake(socket)
+            .and_then(|socket| {
+                create_connection(socket)
+            })
+            .and_then(|(socket, serv_socket)| {
+                Transfer::new(socket, serv_socket)
+            })
+            .map_err(|e| println!("Server Error: {}", e));
 
-        tokio::spawn(peer.map_err(|e| println!("Client Error: {:?}", e)))
+        tokio::spawn(serv)
     });
 
     tokio::run(f);
